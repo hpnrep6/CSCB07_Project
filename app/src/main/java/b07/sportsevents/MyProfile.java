590c3eab2a3e33802b2f78b4137b8590e71a76ed
Pedram meskoub <pedramhaqiqi@Pedrams-MacBook-Pro.local>
2022-08-09 13:41:43 -0400

    menu fix

--- app/src/main/java/b07/sportsevents/MyProfile.java
+++ app/src/main/java/b07/sportsevents/MyProfile.java
@@ -28,17 +28,22 @@ public class MyProfile extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.My_Events:
+                Intent in = new Intent(this, ViewEvents.class);
+                in.putExtra("filter", ViewEvents.Filter.USER);
+                startActivity(in);
             case R.id.Upcoming_events:
-                Intent ve = new Intent(this, ViewEvents.class);
-                startActivity(ve);
+                Intent intent = new Intent(this, ViewEvents.class);
+                intent.putExtra("filter", ViewEvents.Filter.ALL);
+                startActivity(intent);
                 return true;
             case R.id.My_Profile:
                 Intent mp = new Intent(this, MyProfile.class);
                 startActivity(mp);
                 return true;
             case R.id.Schedule_Events:
-                Intent se = new Intent(this, ViewVenues.class);
-                startActivity(se);
+                Intent i = new Intent(this, ViewVenues.class);
+                i.putExtra("filter", ViewVenues.Filter.ALL);
+                startActivity(i);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
